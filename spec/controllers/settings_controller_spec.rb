bb91b51e282436c54de9817204cd36f597c28bc0
Dmitriy Pribytkovskiy <dr.trem86@gmail.com>
2019-02-21 18:01:52 +0200

    describe SettingsController

---
+++ spec/controllers/settings_controller_spec.rb
@@ -0,0 +1,88 @@
+require 'rails_helper'
+
+RSpec.describe SettingsController, type: :controller do
+  let(:user) { create(:user) }
+  let(:users_address_shipping) { create(:users_address, :shipping) }
+  let(:users_address_billing) { create(:users_address, :billing) }
+
+  before do
+    sign_in user
+    request.env['devise.mapping'] = Devise.mappings[:user]
+  end 
+
+  describe 'GET #index' do
+    before { get :index, params: { user_id: user.id } }
+
+    it 'renders settings/index template' do
+      expect(response).to render_template 'settings/index'
+    end
+
+    it 'responds successfully with an HTTP 200 status code' do
+      expect(response).to be_successful
+      expect(response).to have_http_status(200)
+    end
+
+    it 'assigns @address' do
+      expect(assigns(:address)).not_to be_nil
+    end
+  end
+
+  describe 'PATCH #update' do
+    let(:bed_phone) { 123 }
+
+    describe 'when address does not exist' do
+      it 'save address' do
+        expect { 
+          patch :update, params: { address_form: {
+            first_name:                   users_address_billing.first_name,
+            last_name:                    users_address_billing.last_name,
+            address:                      users_address_billing.address,
+            city:                         users_address_billing.city,
+            zip:                          users_address_billing.zip,
+            country:                      users_address_billing.country,
+            phone:                        users_address_billing.phone,
+            shipping_first_name:          users_address_shipping.first_name,
+            shipping_last_name:           users_address_shipping.last_name,
+            shipping_address:             users_address_shipping.address,
+            shipping_city:                users_address_shipping.city,
+            shipping_zip:                 users_address_shipping.zip,
+            shipping_country:             users_address_shipping.country,
+            shipping_phone:               users_address_shipping.phone
+          }, user_id: users_address_shipping.addressable_id, id: users_address_shipping.addressable_id }
+        }.to change(Address, :count).by(2)
+      end
+
+      it 'redirect to the edit' do
+        patch :update, params: { address_form: {
+          first_name:                   users_address_billing.first_name,
+          last_name:                    users_address_billing.last_name,
+          address:                      users_address_billing.address,
+          city:                         users_address_billing.city,
+          zip:                          users_address_billing.zip,
+          country:                      users_address_billing.country,
+          phone:                        bed_phone,
+          shipping_first_name:          users_address_shipping.first_name,
+          shipping_last_name:           users_address_shipping.last_name,
+          shipping_address:             users_address_shipping.address,
+          shipping_city:                users_address_shipping.city,
+          shipping_zip:                 users_address_shipping.zip,
+          shipping_country:             users_address_shipping.country,
+          shipping_phone:               bed_phone
+        }, user_id: users_address_shipping.addressable_id, id: user.id }
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'when address exist' do
+      before do 
+        users_address_shipping.addressable_id = user.id
+        users_address_billing.addressable_id = user.id
+        get :index, params: { user_id: users_address_shipping.addressable_id, id: user.id }
+      end
+
+      it 'view user address' do
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
+end
