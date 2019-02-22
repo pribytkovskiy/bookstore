e41ffe24781dac51d06dd5e08c5db96607af3943
Dmitriy Pribytkovskiy <dr.trem86@gmail.com>
2019-02-22 13:50:40 +0200

    feature delivery step

--- spec/features/checkout/delivery_spec.rb
+++ spec/features/checkout/delivery_spec.rb
@@ -1,21 +1,23 @@
 require 'rails_helper'
 
 feature 'delivery step' do
-  let!(:order) { create(:order, :with_items) }
+  let(:user) { create(:user, :with_orders_address) }
+  let(:order) { user.orders.first }
   let!(:delivery) { create(:delivery) }
 
   before do
     sign_in create(:user)
     order.state = :delivery_method
+    order.save
+    page.set_rack_session(order_id: order.id)
     visit order_path(id: order.id)
   end
 
-  it "show delivery info" do
-    expect(page).to have_content(delivery.price)
-    expect(page).to have_content(delivery.days)
+  it 'show delivery info' do
+    expect(page).to have_content(I18n.t('orders.address_form.regular_delivery'))
   end
 
-  it "save delivery" do
+  it 'save delivery' do
     find('.radio-text', match: :first).click
     click_button(I18n.t('orders.address_form.save_and_continue'))
     expect(order.reload.delivery_id).to eq(delivery.id)
