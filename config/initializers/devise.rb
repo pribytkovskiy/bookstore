485a439bba07b969ff387cb50478016b08946846
Dmitriy Pribytkovskiy <dr.trem86@gmail.com>
2019-03-12 13:31:54 +0200

    add oauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :facebook, '1893916007558817', '5d9022504e8c28528d32791b2fc0d034'
+  config.omniauth :facebook, ENV['OMNIAUTH_ID'], ['OMNIAUTH_SECRET']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
