892ba288679e7459e8ddf4e7f8b44572223ac92c
Dmitriy Pribytkovskiy <dr.trem86@gmail.com>
2019-03-12 12:28:02 +0200

    fix test after rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,9 @@ class User < ApplicationRecord
 
   def self.new_with_session(params, session)
     super.tap do |user|
-      return unless data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
-      
-      user.email = data['email'] if user.email.blank?
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
     end
   end
 
