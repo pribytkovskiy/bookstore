8e421bd92081f66fc11d5745d0a6f4171a7d81f7
Dmitriy Pribytkovskiy <dr.trem86@gmail.com>
2019-03-13 10:39:05 +0200

    check omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -17,7 +17,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-  config.omniauth :facebook, ENV['OMNIAUTH_ID'], ['OMNIAUTH_SECRET']
+  config.omniauth :facebook, ENV['OMNIAUTH_ID'], ENV['OMNIAUTH_SECRET']
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
