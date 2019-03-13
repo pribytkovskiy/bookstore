53652fd1939bf18013d1001fe093f2f77727848c
Dmitriy Pribytkovskiy <dr.trem86@gmail.com>
2019-03-13 10:48:17 +0200

    check omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,6 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.first_name = auth.info.name.split(' ')[0]
-      user.last_name = auth.info.name.split(' ')[1]
       user.pictures = auth.info.image
       user.save
     end
