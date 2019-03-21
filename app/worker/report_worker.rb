0e74ebc3efcadaee71580e1516e06ecbfbc35fcf
Dmitriy Pribytkovskiy <dr.trem86@gmail.com>
2019-03-21 15:10:19 +0200

    add sidekiq

--- app/worker/report_worker.rb
+++ app/worker/report_worker.rb
@@ -2,7 +2,7 @@ class ReportWorker
   include Sidekiq::Worker
   sidekiq_options retry: false
 
-  def perform(user, order)
+  def perform(user_email, order_id)
     OrderMailer.complete_email(user_email, order_id).deliver_later
   end
 end
